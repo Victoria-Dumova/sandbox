339e9b2a04c4b20cf95db8caaeb0ba77a68c8129
Victoria.Dumova <victoria.dumova@jetbrains.com>
2025-02-10 20:20:23 +0100

    non-english output test files added

---
+++ src/terminal/sandbox/InputTest.java
@@ -0,0 +1,17 @@
+package terminal.sandbox;
+
+import java.util.Scanner;
+
+class InputTest {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter smth: ");
+        String smth = input.next();
+        System.out.println("You entered " + smth);
+
+        // closing the scanner object
+        input.close();
+    }
+}
