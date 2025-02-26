cd0728f7ac6abe5bdd841a07d1cec3e44a57bb15
Victoria.Dumova <victoria.dumova@jetbrains.com>
2025-02-26 17:31:38 +0100

    _

--- src/terminal/sandbox/OutputTest.java
+++ src/terminal/sandbox/OutputTest.java
@@ -4,5 +4,8 @@ public class OutputTest {
     public static void main(String[] args) {
         System.out.println("이름을 입력하세요: ");
         System.out.println("ㅋㅌㅊㅍ");
+        System.out.println("ㅋㅌㅊㅍ");
+        System.out.println("ㅋㅌㅊㅍ");
+        System.out.println("ㅋㅌㅊㅍ");
     }
 }
